99cdcdf0fbc3f1197d135e7494652a1387430fdb
Ryan Sam <ryansam.inquiries@gmail.com>
2021-12-31 04:37:42 -0600

    simple contrast algo with opencv

--- main.cpp
+++ main.cpp
@@ -1,34 +1,19 @@
 // #include <filesystem>
 #include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/core.hpp"
 #include "CImg.h"
 
 template <class T>
 constexpr
 std::string_view
-type_name()
-{
-    using namespace std;
-#ifdef __clang__
-    string_view p = __PRETTY_FUNCTION__;
-    return string_view(p.data() + 34, p.size() - 34 - 1);
-#elif defined(__GNUC__)
-    string_view p = __PRETTY_FUNCTION__;
-#  if __cplusplus < 201402
-    return string_view(p.data() + 36, p.size() - 36 - 1);
-#  else
-    return string_view(p.data() + 49, p.find(';', 49) - 49);
-#  endif
-#elif defined(_MSC_VER)
-    string_view p = __FUNCSIG__;
-    return string_view(p.data() + 84, p.size() - 84 - 7);
-#endif
-}
+type_name();
 
 size_t truncate(size_t value);
 
-int main() {
-  cimg_library::CImg<float> skittleImg("./colors/lena_condec.jpeg");
-  // cimg_library::CImg<float> imgBuffer("./colors/original_green.jpg");
+int main(int argc, char** argv) {
+  cimg_library::CImg<float> skittleImg("./colors/lenna_soderberg.jpeg");
 
   int width = skittleImg.width();
   int height = skittleImg.height();
@@ -50,70 +35,65 @@ int main() {
   //       }
 
   // enhancing contrast
-  // typeof skittleImg(col, row, 0, 0) is unsigned char &
-  float minIntensity = 1.0f;
-  float maxIntensity = 0.0f;
-  cimg_library::CImg<float> imgBuffer = skittleImg.get_RGBtoHSI();
-  for (int row = 0; row < height; row++)
-        for (int col = 0; col < width; col++) {
-            const auto I = imgBuffer(col, row, 0, 2);
-            minIntensity = std::min((float)I, minIntensity);
-            maxIntensity = std::max((float)I, maxIntensity);
+  cv::CommandLineParser parser( argc, argv, "{@input | ./colors/lenna_soderberg.jpeg | input image}" );
+  cv::Mat image = cv::imread( cv::samples::findFile( parser.get<cv::String>( "@input" ) ) );
+  if( image.empty() )
+  {
+    std::cout << "Could not open or find the image!\n" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <Input image>" << std::endl;
+    return -1;
+  }
+
+  cv::Mat imageBuffer = cv::Mat::zeros( image.size(), image.type() );
+
+  double alpha = 1.8; /*< Simple contrast control */
+  int beta = 0;     /*< Simple brightness control */
+
+  for( int row = 0; row < image.rows; row++ ) {
+        for( int col = 0; col < image.cols; col++ ) {
+            for( int channel = 0; channel < image.channels(); channel++ ) {
+                imageBuffer.at<cv::Vec3b>(row,col)[channel] =
+                  cv::saturate_cast<uchar>( alpha * image.at<cv::Vec3b>(row,col)[channel] + beta );
+            }
         }
+  }
 
-  for (int row = 0; row < height; row++)
-        for (int col = 0; col < width; col++) {
-          const auto I = imgBuffer(col, row, 0, 2);
-          const auto newIntensity = (((float)I - minIntensity) / (maxIntensity - minIntensity));
-          std::cout << "old Intensity " << (float)I << std::endl;
-          std::cout << "new Intensity " << newIntensity << std::endl;
-          imgBuffer(col, row, 0, 2) = newIntensity;
-        }
-
-  std::cout << "min " << minIntensity << std::endl;
-  std::cout << "max " << maxIntensity << std::endl;
-
-  cimg_library::CImg<float> outputImg = imgBuffer.get_HSItoRGB();
-
-  // for (int row = 0; row < height; row++)
-  //       for (int col = 0; col < width; col++) {
-  //
-  //         const int contrast = 5;
-  //         float factor = (259.0f * (contrast + 255.0f)) / (255.0f * (259.0f - contrast));
-  //
-  //         const auto R = skittleImg(col, row, 0, 0);
-  //         const auto G = skittleImg(col, row, 0, 1);
-  //         const auto B = skittleImg(col, row, 0, 2);
-  //
-  //         // std::cout << (int)R << std::endl;
-  //
-  //         size_t newRed   = truncate((size_t)(factor * ((int)R - 128) + 128));
-  //         size_t newGreen = truncate((size_t)(factor * ((int)G - 128) + 128));
-  //         size_t newBlue  = truncate((size_t)(factor * ((int)B - 128) + 128));
-  //
-  //         imgBuffer(col, row, 0, 0) = (unsigned char)newRed;
-  //         imgBuffer(col, row, 0, 1) = (unsigned char)newGreen;
-  //         imgBuffer(col, row, 0, 2) = (unsigned char)newBlue;
-  //
-  //       }
+  imshow("Original Image", image);
+  imshow("New Image", imageBuffer);
+  cv::waitKey();
 
   // convert to gray scale
   //imgBuffer.RGBtoYCbCr().channel(0);
 
 
 
-
-
-
   // Debugging
-  cimg_library::CImgDisplay main_disp(skittleImg, "Green Skittle");
-  cimg_library::CImgDisplay alt_disp(outputImg, "Edited");
-  main_disp.display(skittleImg);
-  alt_disp.display(outputImg);
 
   std::getchar();
 
   return 0;
+}
+
+template <class T>
+constexpr
+std::string_view
+type_name()
+{
+    using namespace std;
+#ifdef __clang__
+    string_view p = __PRETTY_FUNCTION__;
+    return string_view(p.data() + 34, p.size() - 34 - 1);
+#elif defined(__GNUC__)
+    string_view p = __PRETTY_FUNCTION__;
+#  if __cplusplus < 201402
+    return string_view(p.data() + 36, p.size() - 36 - 1);
+#  else
+    return string_view(p.data() + 49, p.find(';', 49) - 49);
+#  endif
+#elif defined(_MSC_VER)
+    string_view p = __FUNCSIG__;
+    return string_view(p.data() + 84, p.size() - 84 - 7);
+#endif
 }
 
 size_t truncate(size_t value)
